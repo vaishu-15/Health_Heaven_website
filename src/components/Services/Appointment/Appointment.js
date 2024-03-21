be54edc676374df3817edbfc1c0b8dae17e25ef4
vaishu-15 <vaishali15yadav@gmail.com>
2024-03-21 09:27:59 +0530

    health website

--- src/components/Services/Appointment/Appointment.js
+++ src/components/Services/Appointment/Appointment.js
@@ -1,97 +1,173 @@
-import { DateTimePicker, LocalizationProvider, MobileDateTimePicker } from '@mui/lab';
-import { Box, Button, Container, FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
-import React from 'react';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import swal from 'sweetalert';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import useAuth from '../../../Hooks/useAuth';
-
+import {
+  DateTimePicker,
+  LocalizationProvider,
+  MobileDateTimePicker,
+} from "@mui/lab";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import swal from "sweetalert";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import useAuth from "../../../Hooks/useAuth";
 
 const Appointment = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
-    const [clearedDate, setClearedDate] = React.useState(null);
-    const [value, setValue] = React.useState(new Date());
+  const [docName, setDocName] = useState("");
+  const [name, setName] = useState(user.displayName);
+  const [email, setEmail] = useState(user.email);
+  const [appointmentDate, setAppointmentDate] = useState(new Date());
+  const [problemType, setProblemType] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
 
-    // doctor name function
-    const [docName, setDocName] = React.useState('');
+  const handleDocNameChange = (event) => {
+    setDocName(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setDocName(event.target.value);
-    };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-    //swal alert
-    const swalAlert = () => {
-        return swal("Your Appointment is Done You will Receive a mail ASAP.", {
-            button: false,
-            icon: "success"
-        });
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleAppointmentDateChange = (newValue) => {
+    setAppointmentDate(newValue);
+  };
+
+  const handleProblemTypeChange = (event) => {
+    setProblemType(event.target.value);
+  };
+
+  const validateForm = () => {
+    return docName !== "" && name !== "" && email !== "" && problemType !== "";
+  };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      swalAlert();
+    } else {
+      // Handle form submission error
     }
-    return (
-        <Box id='appointment'
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}>
-            <Container maxWidth="xl">
-                <Typography variant='h6' sx={{
-                    mt: 5, mb: 5
-                }}>Select your time and data for Appointment</Typography>
+  };
 
+  const swalAlert = () => {
+    return swal("Your Appointment is Done You will Receive a mail ASAP.", {
+      button: false,
+      icon: "success",
+    });
+  };
 
-                {/* set doctor name */}
-                <FormControl sx={{ mb: 5, minWidth: '50%' }}>
-                    <InputLabel id="demo-simple-select-autowidth-label">Select Doctor Name</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-autowidth-label"
-                        id="demo-simple-select-autowidth"
-                        value={docName}
-                        onChange={handleChange}
-                        autoWidth
-                        label="Select Doctor Name"
-                    >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem value={10}>Lee S. Williamson</MenuItem>
-                        <MenuItem value={11}>Greg S. Grinstead</MenuItem>
-                        <MenuItem value={12}>Roger K. Jackson</MenuItem>
-                        <MenuItem value={13}>Frank T. Grimsley</MenuItem>
-                        <MenuItem value={14}>Rudolph V. Spitler</MenuItem>
-                        <MenuItem value={16}>Erik R. Faulkner</MenuItem>
-                        <MenuItem value={17}>Phillip L. Williams</MenuItem>
-                        <MenuItem value={18}>Johnny R. Atterberry</MenuItem>
-                        <MenuItem value={19}>Michael I. Johnson</MenuItem>
-                    </Select>
-                </FormControl>
-                <TextField sx={{ mb: 2 }} value={user.displayName} fullWidth label="Your Name" id="fullWidth" />
-                <TextField sx={{ mb: 2 }} value={user.email} fullWidth label="Your Mail" id="fullWidth" />
+  return (
+    <Box
+      id="appointment"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Typography
+          variant="h6"
+          sx={{
+            mt: 5,
+            mb: 5,
+          }}
+        >
+          Select your time and data for Appointment
+        </Typography>
 
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <Stack spacing={3}>
-                        <MobileDateTimePicker
-                            value={value}
-                            onChange={(newValue) => {
-                                setValue(newValue);
-                            }}
-                            label="Appointment Date"
-                            onError={console.log}
-                            minDate={new Date('2018-01-01T00:00')}
-                            inputFormat="yyyy/MM/dd hh:mm a"
-                            mask="___/__/__ __:__ _M"
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </Stack>
-                </LocalizationProvider>
+        <FormControl sx={{ mb: 5, minWidth: "50%" }}>
+          <InputLabel id="demo-simple-select-autowidth-label">
+            Select Doctor Name
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+            value={docName}
+            onChange={handleDocNameChange}
+            autoWidth
+            label="Select Doctor Name"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Mohit Tondon</MenuItem>
+            <MenuItem value={11}>Anurag Mahrotra</MenuItem>
+            <MenuItem value={12}>Ragesh Rastogi</MenuItem>
+            <MenuItem value={13}>Ashima Malhotra</MenuItem>
+            <MenuItem value={14}>Gesu Malhotra</MenuItem>
+            <MenuItem value={16}>Neha Chandra</MenuItem>
+            <MenuItem value={17}>Rohit Garg</MenuItem>
+            <MenuItem value={18}>Johnny R. Atterberry</MenuItem>
+            <MenuItem value={19}>Poonam Singh</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          sx={{ mb: 2 }}
+          value={name}
+          onChange={handleNameChange}
+          fullWidth
+          label="Your Name"
+          id="fullName"
+        />
+        <TextField
+          sx={{ mb: 2 }}
+          value={email}
+          onChange={handleEmailChange}
+          fullWidth
+          label="Your Mail"
+          id="email"
+        />
 
-                <TextField sx={{ mt: 2, mb: 2 }} fullWidth label="Problem type" id="fullWidth" />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Stack spacing={3}>
+            <MobileDateTimePicker
+              value={appointmentDate}
+              onChange={handleAppointmentDateChange}
+              label="Appointment Date"
+              onError={console.log}
+              minDate={new Date("2018-01-01T00:00")}
+              inputFormat="yyyy/MM/dd hh:mm a"
+              mask="___/__/__ __:__ _M"
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </Stack>
+        </LocalizationProvider>
 
-                <Button sx={{ p: 1, mt: 2, mb: 5 }} onClick={swalAlert} fullWidth
-                    variant="contained"><AddCircleIcon /> Confirm</Button>
-            </Container>
-        </Box>
-    );
+        <TextField
+          sx={{ mt: 2, mb: 2 }}
+          value={problemType}
+          onChange={handleProblemTypeChange}
+          fullWidth
+          label="Problem type"
+          id="problemType"
+        />
+
+        <Button
+          sx={{ p: 1, mt: 2, mb: 5 }}
+          onClick={handleSubmit}
+          fullWidth
+          variant="contained"
+        >
+          <AddCircleIcon /> Confirm
+        </Button>
+      </Container>
+    </Box>
+  );
 };
 
 export default Appointment;
